66be0b064b27a5b2363c8b840af1bc9339d38150
cleidson <cleidsonjds@hotmail.com>
2023-09-12 01:16:00 -0300

    Plano Operadora

---
+++ primeiroProjeto/src/edu/cleidson/controleFluxo/PlanoOperadora.java
@@ -0,0 +1,19 @@
+package edu.cleidson.controleFluxo;
+
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "B"; //M / T / B
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M":{
+                System.out.println("Whats e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
