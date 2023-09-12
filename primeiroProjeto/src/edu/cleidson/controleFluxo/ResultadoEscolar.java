696ad3f90893fcdb2222ab8ce49ccffa9273703d
cleidson <cleidsonjds@hotmail.com>
2023-09-12 01:01:19 -0300

    ALternativas ao IF ELSE ELSE IF

--- primeiroProjeto/src/edu/cleidson/controleFluxo/ResultadoEscolar.java
+++ primeiroProjeto/src/edu/cleidson/controleFluxo/ResultadoEscolar.java
@@ -8,6 +8,16 @@ public class ResultadoEscolar {
         else if(nota >= 5 && nota <7)
             System.out.println("Recuperação");
         else
-            System.out.println("Reprovado");
+            System.out.println("Reprovado"); 
+
+        //int nota = 7;
+        //String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        //System.out.println(resultado);
+
+        //int nota = 6;
+        //String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        //System.out.println(resultado);
+
     }
 }
