3213724b1df0bf06a1a617dcafe29ba483a99820
cleidson <cleidsonjds@hotmail.com>
2023-09-14 00:07:50 -0300

    Controle de fluxo com TRYCATCH

--- primeiroProjeto/src/MinhaClasse.java
+++ primeiroProjeto/src/MinhaClasse.java
@@ -1,5 +1,11 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá turma, fui executado pelo CMD!");
+        int num = 5, count = 1;
+        do {
+            num += count;
+            System.out.println(num);
+        } while (count <= 3);
+
     }
 }
