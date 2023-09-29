a9f790f0a6ef0f864687b5fba4f887f443fac72d
cleidson <cleidsonjds@hotmail.com>
2023-09-29 13:40:01 -0300

    Desafio-CondicionalmenteRico

---
+++ desafio-CondicionalmenteRico/src/CondicionalRico.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CondicionalRico {
+  public static void main(String[] args) {
+
+    // Entrada de dados
+    Scanner scanner = new Scanner(System.in);
+    int saldoTotal = scanner.nextInt();
+    int valorSaque = scanner.nextInt();
+
+    //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+    if(saldoTotal >= valorSaque){
+     int saldo = saldoTotal - valorSaque;
+      System.out.println("Saque realizado com sucesso. Novo saldo: " + saldo);
+    }else{
+      System.out.println("Saldo insuficiente. Saque nao realizado!");
+    }
+  }
+}
