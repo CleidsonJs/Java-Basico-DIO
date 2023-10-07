45aaebccb1ba29c63de7f93b3aad352dfd6ea3fa
CleidsonJs <Cleidsonjds@hotmail.com>
2023-10-06 21:29:30 -0300

    Encapsulamento com MSNMessenger

---
+++ MSNMessenger/src/MSNMessenger/Computador.java
@@ -0,0 +1,11 @@
+package MSNMessenger;
+
+public class Computador {
+    public static void main(String[] args) {
+        //abrindo MSN Messenger
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
